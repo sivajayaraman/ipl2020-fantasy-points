b858e23ddb03a6fe12f7d573bce95d84e55088d9
sivajayaraman <srksabarish12@gmail.com>
2020-10-02 20:47:24 +0530

    Updates

---
+++ precache-manifest.48faea303c6adc50b8f11e7b1d4e4c25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea9ec7e76b9ed7878a53",
+    "url": "/ipl2020-fantasy-points/static/css/main.13f492f7.chunk.css"
+  },
+  {
+    "revision": "ea9ec7e76b9ed7878a53",
+    "url": "/ipl2020-fantasy-points/static/js/main.ea9ec7e7.chunk.js"
+  },
+  {
+    "revision": "355a456b32d31e47e786",
+    "url": "/ipl2020-fantasy-points/static/js/runtime~main.355a456b.js"
+  },
+  {
+    "revision": "4b8567f351f996ec5fb0",
+    "url": "/ipl2020-fantasy-points/static/css/2.f38c0f1e.chunk.css"
+  },
+  {
+    "revision": "4b8567f351f996ec5fb0",
+    "url": "/ipl2020-fantasy-points/static/js/2.4b8567f3.chunk.js"
+  },
+  {
+    "revision": "bcb428f27877ad72ed45ea433b428137",
+    "url": "/ipl2020-fantasy-points/index.html"
+  }
+];

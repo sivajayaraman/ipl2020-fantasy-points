21b8b914b874cc599ab2c1f2598b49f6d3818fc6
sivajayaraman <srksabarish12@gmail.com>
2020-10-04 10:20:43 +0530

    Updates

--- precache-manifest.e83c23027ea33ed64c55e8843c40ee6e.js
+++ precache-manifest.e83c23027ea33ed64c55e8843c40ee6e.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "ea9ec7e76b9ed7878a53",
+    "revision": "9cdae64ff72814d2f2ad",
     "url": "/ipl2020-fantasy-points/static/css/main.13f492f7.chunk.css"
   },
   {
-    "revision": "ea9ec7e76b9ed7878a53",
-    "url": "/ipl2020-fantasy-points/static/js/main.ea9ec7e7.chunk.js"
+    "revision": "9cdae64ff72814d2f2ad",
+    "url": "/ipl2020-fantasy-points/static/js/main.9cdae64f.chunk.js"
   },
   {
     "revision": "355a456b32d31e47e786",
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/ipl2020-fantasy-points/static/js/2.4b8567f3.chunk.js"
   },
   {
-    "revision": "bcb428f27877ad72ed45ea433b428137",
+    "revision": "990cf4a6cd3b7d06da61b3ae6044b537",
     "url": "/ipl2020-fantasy-points/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ipl2020-fantasy-points/precache-manifest.48faea303c6adc50b8f11e7b1d4e4c25.js"
+  "/ipl2020-fantasy-points/precache-manifest.e83c23027ea33ed64c55e8843c40ee6e.js"
 );
 
 workbox.clientsClaim();
